6225467860847836de86717f5dd349188fbe6f59
Rustem Sharipov <sharipov.rustem@gmail.com>
2025-05-20 11:27:35 -0500

    Demostrate manual routing with "switch" statement

--- main.go
+++ main.go
@@ -5,18 +5,26 @@ import (
 	"net/http"
 )
 
-// func homeHandler(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-// 	fmt.Fprint(w, "<h1>Welcome to my web site built with Go !</h1>")
-// }
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my web site built with Go !</h1>")
+}
 
-// func contactHandler(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-// 	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch email me at <a href=\"mailto:sharipov.rustem@gmail.com\">sharipov.rustem@gmail.com</a></p>")
-// }
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch email me at <a href=\"mailto:sharipov.rustem@gmail.com\">sharipov.rustem@gmail.com</a></p>")
+}
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, r.URL.Path)
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// TO DO:
+		// handle the page not found error
+	}
 }
 
 func main() {
