0557fed94f3c452fb4f81231cc9d03034ea35cf4
Rustem Sharipov <sharipov.rustem@gmail.com>
2025-05-20 11:09:03 -0500

    Added "contact" handler/page and also generic "path" handler to discover Request properties.

--- main.go
+++ main.go
@@ -5,12 +5,24 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my web site built with Go !</h1>")
+// func homeHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	fmt.Fprint(w, "<h1>Welcome to my web site built with Go !</h1>")
+// }
+
+// func contactHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch email me at <a href=\"mailto:sharipov.rustem@gmail.com\">sharipov.rustem@gmail.com</a></p>")
+// }
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.URL.Path)
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	// http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on: 3000...")
 	http.ListenAndServe(":3000", nil)
 }
