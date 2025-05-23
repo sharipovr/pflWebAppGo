0cc8d3791b049cf4ce0157ab5970d40ed7dfbaa7
Rustem Sharipov <sharipov.rustem@gmail.com>
2025-05-23 14:45:29 -0500

    Added FAQ page. Section2 is completed.

--- main.go
+++ main.go
@@ -15,6 +15,28 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch email me at <a href=\"mailto:sharipov.rustem@gmail.com\">sharipov.rustem@gmail.com</a></p>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>Frequently Asked Questions</h1>
+<ul>
+  <li>
+    <h3>Q: Is there a free version?</h3>
+    <p>A: Yes! We offer a free trial for 30 days on any paid plans.</p>
+  </li>
+
+  <li>
+    <h3>Q: What are your support hours?</h3>
+    <p>A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.</p>
+  </li>
+
+  <li>
+    <h3>Q: How do I contact support?</h3>
+    <p>A: Email us – <a href="mailto:support@rustems.dev">support@rustems.dev</a></p>
+  </li>
+</ul>
+	`)
+}
+
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
 // 	case "/":
@@ -34,6 +56,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
